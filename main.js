8bc9b919bdcf9260af7f0567ababd3b6667b4959
mitchell1998 <1744849839@qq.com>
2021-03-28 19:29:12 +0800

    add and delete

--- main.js
+++ main.js
@@ -1,35 +1,97 @@
+
+
 function copy(obj) {
     var copyVal = Object.assign({}, obj);
     return copyVal;
 };
 
+var filters = {
+    all: function (todos) {
+        return todos;
+    },
+    uncompleted: function (todos) {
+        return todos.filter(function (todo) {
+            return !todo.completed;
+        })
+    },
+    completed: function (todos) {
+        return todos.filter(function (todo) {
+            return todo.completed;
+        })
+    }
+}
+
+
 var app = new Vue({
     el: '#app',
     data: {
-        inputText: {
-            time: '', content: '', completed: false, detail: ''
-        },
-        todoList: [
-            { time: '2020年3月16日', content: '洗衣服', completed: false, detail: '' },
-            { time: '2020年3月26日', content: '学习Vue', completed: false, detail: '清单' },
-            { time: '2020年3月25日', content: '买纸巾', completed: false, detail: '去教超' },
-        ]
+        inputText: {},
+        todoList: [],
+        lastId: 0,
+        visibility: "all",
     },
     methods: {
+        getId: function () {
+            this.lastId += 1;
+            ms.set('lastId', this.lastId);
+            return this.lastId;
+        },
+        findById: function (id) {
+            return this.todoList.findIndex(function (item) {
+                return item.id == id;
+            })
+        },
+        resetInput() {
+            this.inputText = copy({});
+        },
         addToDo: function () {
             let tempContent = this.inputText.content;
             if (!tempContent && tempContent !== 0) return;
+
             let tempText = copy(this.inputText);
+            tempText.id = this.getId();
+            tempText.completed = false;
             //加入数据
             this.todoList.push(tempText);
             //清空数据
-            this.inputText.time = '';
-            this.inputText.content = '';
-            this.inputText.completed = false;
-            this.inputText.detail = '';
+            this.resetInput();
         },
-        deleteToDo: function () {
+        deleteToDo: function (id) {
+            var index = this.findById(id);
+            this.todoList.splice(index, 1);
+        },
+        toggleComplete: function (id) {
+            var index = this.findById(id);
+            Vue.set(this.todoList[index], 'completed', !this.todoList[index].completed);
+        },
 
+    },
+    mounted: function () {
+        this.todoList = ms.get('todoList') || this.todoList;
+        this.lastId = ms.get('lastId') || this.lastId;
+    },
+    watch: {
+        todoList: {
+            deep: true,
+            handler: function (newVal, oldVal) {
+                ms.set('todoList', newVal);
+            }
         }
+    },
+    computed: {
+
     }
-})
+});
+
+function onHashChange() {
+    var visibility = window.location.hash.replace(/#\/?/, "");//正则表达式，空字符替换#
+    if (filters[visibility]) {
+        app.visibility = visibility;
+    } else {
+        window.location.hash = "";
+        app.visibility = "all";
+    }
+};
+
+window.addEventListener("hashchange", onHashChange);
+onHashChange();
